e6469cb85e62d91d7cbb855f09f3dc37b46062ad
Yang Yingliang <yangyingliang@huawei.com>
2015-09-24 17:32:13 +0800

    genirq: Introduce generic irq migration for cpu hotunplug
    ARM and ARM64 have almost identical code for migrating interrupts on
    cpu hotunplug. Provide a generic version which can be used by both.
    
    The new code addresses a shortcoming in the ARM[64] variants which
    fails to update the affinity change in some cases. The solution for
    this is to use the core function irq_do_set_affinity() instead of open
    coding it.
    
    [ tglx: Added copyright notice and license boilerplate. Rewrote
      	subject and changelog. ]
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Acked-by: Russell King - ARM Linux <linux@arm.linux.org.uk>
    Cc: Jiang Liu <jiang.liu@linux.intel.com>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: Hanjun Guo <hanjun.guo@linaro.org>
    Cc: <linux-arm-kernel@lists.infradead.org>
    Link: http://lkml.kernel.org/r/1443087135-17044-2-git-send-email-yangyingliang@huawei.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -409,6 +409,8 @@ extern void irq_cpu_offline(void);
 extern int irq_set_affinity_locked(struct irq_data *data,
 				   const struct cpumask *cpumask, bool force);
 
+extern void irq_migrate_all_off_this_cpu(void);
+
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_PENDING_IRQ)
 void irq_move_irq(struct irq_data *data);
 void irq_move_masked_irq(struct irq_data *data);
