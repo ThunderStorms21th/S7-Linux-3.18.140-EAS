90f9d8e658921c2e487be51efcf67d9665b8062f
Olav Haugan <ohaugan@codeaurora.org>
2016-11-03 15:10:57 -0700

    BACKPORT: sched: Ensure watchdog is enabled before disabling
    There is a race between watchdog being enabled by hotplug and core
    isolation disabling the watchdog. When a CPU is hotplugged in and
    the hotplug lock has been released the watchdog thread might not
    have run yet to enable the watchdog.  We have to wait for the
    watchdog to be enabled before proceeding.
    
    Change-Id: I88f73603b6d389a46f8e819d9b490091d5ba4fe9
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -412,6 +412,7 @@ extern unsigned int  softlockup_panic;
 void lockup_detector_init(void);
 extern void watchdog_enable(unsigned int cpu);
 extern void watchdog_disable(unsigned int cpu);
+extern bool watchdog_configured(unsigned int cpu);
 #else
 static inline void touch_softlockup_watchdog(void)
 {
@@ -430,6 +431,14 @@ static inline void watchdog_enable(unsigned int cpu)
 }
 static inline void watchdog_disable(unsigned int cpu)
 {
+}
+static inline bool watchdog_configured(unsigned int cpu)
+{
+	/*
+	 * Predend the watchdog is always configured.
+	 * We will be waiting for the watchdog to be enabled in core isolation
+	 */
+	return true;
 }
 #endif
 
