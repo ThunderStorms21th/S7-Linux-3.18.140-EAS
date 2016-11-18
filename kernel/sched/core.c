569eb36ee5bae8ed40a1f2217118c21881479c56
Olav Haugan <ohaugan@codeaurora.org>
2016-11-17 18:31:33 -0800

    BACKPORT: sched: Ensure proper synch between isolation, hotplug, and suspend
    Isolation code needs to be synchronized with both hotplug and suspend.
    Ensure this by taking the lock that is taken by both paths and ensure
    hotplug notifiers are processed for suspend/resume.
    
    Change-Id: I663588cfd2f9e3972b9adc1a10887ef36cd70c57
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5596,7 +5596,7 @@ int sched_isolate_cpu(int cpu)
 	if (trace_sched_isolate_enabled())
 		start_time = sched_clock();
 
-	lock_device_hotplug();
+	cpu_maps_update_begin();
 
 	cpumask_andnot(&avail_cpus, cpu_online_mask, cpu_isolated_mask);
 
@@ -5645,7 +5645,7 @@ int sched_isolate_cpu(int cpu)
 	sched_update_group_capacities(cpu);
 
 out:
-	unlock_device_hotplug();
+	cpu_maps_update_done();
 	trace_sched_isolate(cpu, cpumask_bits(cpu_isolated_mask)[0],
 			    start_time, 1);
 	return ret_code;
@@ -5665,8 +5665,6 @@ int sched_unisolate_cpu_unlocked(int cpu)
 
 	if (trace_sched_isolate_enabled())
 		start_time = sched_clock();
-
-	lock_device_hotplug_assert();
 
 	if (!cpu_isolation_vote[cpu]) {
 		ret_code = -EINVAL;
@@ -5706,9 +5704,9 @@ int sched_unisolate_cpu(int cpu)
 {
 	int ret_code;
 
-	lock_device_hotplug();
+	cpu_maps_update_begin();
 	ret_code = sched_unisolate_cpu_unlocked(cpu);
-	unlock_device_hotplug();
+	cpu_maps_update_done();
 	return ret_code;
 }
 #endif /* CONFIG_HOTPLUG_CPU */
