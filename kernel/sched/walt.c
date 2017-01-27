4b0b52234a5d42f343a1323e9075a274142051e2
Chris Redpath <chris.redpath@arm.com>
2017-01-27 08:12:30 +0000

    sched/walt: Drop arch-specific timer access
    On at least one platform, occasionally the timer providing the wallclock
    was able to be reset/go backwards for at least some time after wakeup.
    
    Accept that this might happen and warn the first time, but otherwise just
    carry on.
    
    Change-Id: Id3164477ba79049561af7f0889cbeebc199ead4e
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    (cherry picked from commit e1ed2c2f275262b94f8203732e2b152b58d2e327)
    (cherry picked from commit 44da378ebc1b676ac6c95cb6310186e795988777)

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -196,10 +196,8 @@ update_window_start(struct rq *rq, u64 wallclock)
 	delta = wallclock - rq->window_start;
 	/* If the MPM global timer is cleared, set delta as 0 to avoid kernel BUG happening */
 	if (delta < 0) {
-		if (arch_timer_read_counter() == 0)
-			delta = 0;
-		else
-			BUG_ON(1);
+		delta = 0;
+		WARN_ONCE(1, "WALT wallclock appears to have gone backwards or reset\n");
 	}
 
 	if (delta < walt_ravg_window)
