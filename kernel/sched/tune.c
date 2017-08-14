70dedc5eb1c41df8cbdf5f7ee0ca8f55fc8b05a9
Patrick Bellasi <patrick.bellasi@arm.com>
2017-08-14 22:22:34 +0900

    DEBUG: schedtune: add tracepoint for SchedTune configuration update Change-Id: I4fc761f304090a91662fd91143dda358b5b91f1d Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1411,6 +1411,12 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	/* Update CPU boost */
 	schedtune_boostgroup_update(st->idx, st->boost);
 
+	trace_sched_tune_config(st->boost,
+			threshold_gains[st->perf_boost_idx].nrg_gain,
+			threshold_gains[st->perf_boost_idx].cap_gain,
+			threshold_gains[st->perf_constrain_idx].nrg_gain,
+			threshold_gains[st->perf_constrain_idx].cap_gain);
+
 out:
 	return err;
 }
