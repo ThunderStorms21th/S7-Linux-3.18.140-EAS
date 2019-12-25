acced03e016fccccb18c30b61391613c74affda2
Samuel Pascua <pascua.samuel.14@gmail.com>
2019-12-25 12:28:51 +0800

    sched, cpufreq, topology: revert almost all of Samsung's changes
    sched/Makefile: add walt.o
    
    kernel: add other changes left out

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -41,6 +41,7 @@ SCHED_FEAT(WAKEUP_PREEMPTION, true)
  */
 SCHED_FEAT(ARCH_POWER, true)
 
+/* TICK */
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(DOUBLE_TICK, false)
 SCHED_FEAT(LB_BIAS, true)
@@ -59,6 +60,8 @@ SCHED_FEAT(TTWU_QUEUE, true)
 SCHED_FEAT(FORCE_SD_OVERLAP, false)
 SCHED_FEAT(RT_RUNTIME_SHARE, true)
 SCHED_FEAT(LB_MIN, false)
+
+SCHED_FEAT(ATTACH_AGE_LOAD, true)
 
 /*
  * Apply the automatic NUMA scheduling policy. Enabled automatically
@@ -83,3 +86,9 @@ SCHED_FEAT(NUMA_FAVOUR_HIGHER, true)
  */
 SCHED_FEAT(NUMA_RESIST_LOWER, false)
 #endif
+
+/*
+ * Energy aware scheduling. Use platform energy model to guide scheduling
+ * decisions optimizing for energy efficiency.
+ */
+SCHED_FEAT(ENERGY_AWARE, false)
