231063e960a761005799c60af010092810321663
ThunderStorms21th <pinakastorm@gmail.com>
2020-05-15 14:51:37 +0200

    smurtfutil_flex: introduce new EAS gov
    from commit : https://github.com/negrroo/LawRun/commit/f28f3888888898fa3f7be07e65063ef229c2c17d#diff-72f6fd8d9ae8cfc4d087a18fcf77827b
    Thx to negrroo.
    
    Modded and edited by nalas for Linux 3.18.xx EAS support.
    
    Signed-off-by: ThunderStorms21th - nalas

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -631,6 +631,9 @@ extern struct cpufreq_governor cpufreq_gov_pwrutilx;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_VLADUTIL)
 extern struct cpufreq_governor cpufreq_gov_vladutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_vladutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMURFUTIL_FLEX)
+extern struct cpufreq_governor cpufreq_gov_smurfutil_flex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smurfutil_flex)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
