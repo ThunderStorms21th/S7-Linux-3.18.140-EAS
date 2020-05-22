8147396d6c6ae2ee69cc463cf525c8b45f36f3f9
Samuel Pascua <pascua.samuel.14@gmail.com>
2020-05-22 15:46:31 +0800

    sched/fair: fix incompatible type warning

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8320,7 +8320,7 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->group_weight = group->group_weight;
 
 		sgs->group_no_capacity = group_is_overloaded(env, sgs);
-		sgs->group_type = group_classify(group, sgs, env);
+		sgs->group_type = group_classify(env, group, sgs);
 	}
 
 	if (sgs->sum_nr_running)
