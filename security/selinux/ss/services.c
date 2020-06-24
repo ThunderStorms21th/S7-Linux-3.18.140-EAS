3e5295e95ee3716f73fa5673790d0637069ba954
Samuel Pascua <sgpascua@ngcp.ph>
2020-06-24 17:09:37 +0800

    selinux: remove audit dependency
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3298,6 +3298,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3320,6 +3321,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
