e1ed5e138b1d38952592de103865fae73cca3082
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-07-06 19:09:42 +0200

    FIXUP: sched/tune: update accouting before CPU capacity
    The SchedTune tasks accounting is used to identify how many tasks are in
    a boostgroup and thus to bias the selection of an OPP based on the
    maximum boost value of the active boostgroups.
    The current implementation however update the accounting after CPU
    capacity has been update. This has two effects:
    a) when we enqueue a boosted task, we do not immediately boost its CPU
    b) when we dequeue a boosted task, we can keep a CPU boosted even if not
       required
    
    This patch change the order of the SchedTune accounting and SchedFreq
    updated to ensure to have always an updated representation of which
    boosted tasks are runnable on a CPU before updating its capacity.
    
    Change-Id: I078ef59c6e8f9cb15a9fb4e0b26fe01f1bc5fed9
    Reported-by: Leo Yan <leo.yan@linaro.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    [ minor merge changes ]
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    
    (cherry picked from commit 15a23e13fa8baab390a40995e9e1d998aa2a0bc0)
    (cherry picked from commit 8d479cd253e45c18763d08114a7772b09ec254ba)
    
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4597,12 +4597,29 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		update_rq_runnable_avg(rq, rq->nr_running);
 		add_nr_running(rq, 1);
 #ifdef CONFIG_SMP
+	    /*
+	     * Update SchedTune accounting.
+	     *
+	     * We do it before updating the CPU capacity to ensure the
+	     * boost value of the current task is accounted for in the
+	     * selection of the OPP.
+	     *
+	     * We do it also in the case where we enqueue a throttled task;
+	     * we could argue that a throttled task should not boost a CPU,
+	     * however:
+	     * a) properly implementing CPU boosting considering throttled
+	     *    tasks will increase a lot the complexity of the solution
+	     * b) it's not easy to quantify the benefits introduced by
+	     *    such a more complex solution.
+	     * Thus, for the time being we go for the simple solution and boost
+	     * also for throttled RQs.
+	     */
+	    schedtune_enqueue_task(p, cpu_of(rq));
+
 		if (!task_new && !rq->rd->overutilized &&
 		    cpu_overutilized(rq->cpu))
 			rq->rd->overutilized = true;
 #endif
-
-		schedtune_enqueue_task(p, cpu_of(rq));
 
 		/*
 		 * We want to trigger a freq switch request only for tasks that
@@ -4684,6 +4701,13 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		sub_nr_running(rq, 1);
 		update_rq_runnable_avg(rq, 1);
 
+	    /*
+	     * Update SchedTune accounting
+	     *
+	     * We do it before updating the CPU capacity to ensure the
+	     * boost value of the current task is accounted for in the
+	     * selection of the OPP.
+	     */
 		schedtune_dequeue_task(p, cpu_of(rq));
 
 		/*
