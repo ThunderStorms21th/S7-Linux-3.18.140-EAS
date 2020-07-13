3580ebe8ff2f8ba59ecfbb45ee93384e99a11796
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-07-13 13:29:34 +0200

    sched: fair - sched: cfs - tweaks for responsiveness v3
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -87,15 +87,15 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
- * (default: 5.00 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 3.00 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 5000000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 5000000ULL;
+unsigned int sysctl_sched_min_granularity = 3000000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 3000000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 15;
+static unsigned int sched_nr_latency = 10;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -105,16 +105,16 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 
 /*
  * SCHED_OTHER wake-up granularity.
- * (default: 5 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 3 msec * (1 + ilog(ncpus)), units: nanoseconds)
  *
  * This option delays the preemption effects of decoupled workloads
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 5000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 5000000UL;
+unsigned int sysctl_sched_wakeup_granularity = 3000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity = 3000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 1000000UL;
+const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
