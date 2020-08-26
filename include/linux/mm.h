cd53d241c0b2cdf225a08feeb214bfed0aa90633
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-26 14:41:32 +0200

    mm: set VM_MIN_READAHEAD to 16kB
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -1916,7 +1916,7 @@ void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
 #define VM_MAX_READAHEAD	512	/* kbytes  org 256 */
-#define VM_MIN_READAHEAD	128	/* kbytes (includes current page) org 16 */
+#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) org 16 */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
